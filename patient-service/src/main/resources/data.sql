8f788572f484c92021fc0f96832bbadb5e72f480
aradgh <arad.ghorreshi@gmail.com>
2025-08-06 14:02:49 +0200

    Add H2 database configuration and seed data for `patient-service` module

---
+++ patient-service/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO patient (first_name, last_name, date_of_birth, gender, mailing_address, phone_number)
+VALUES ('John', 'Doe', '1980-01-01', 'M', '123 rue de Paris', '0600000000');
+
+INSERT INTO patient (first_name, last_name, date_of_birth, gender)
+VALUES ('Jane', 'Smith', '1990-02-15', 'F');
