444ff8e0e587b40d53e811c35642b331b590e9d2
aradgh <arad.ghorreshi@gmail.com>
2025-08-06 16:05:10 +0200

    Initialize `gateway-service` module with Spring Boot setup, Maven wrapper configuration, and basic Spring Cloud Gateway setup. Includes route configuration and dependencies.

--- patient-service/src/main/java/com/openclassrooms/patientservice/model/Patient.java
+++ patient-service/src/main/java/com/openclassrooms/patientservice/model/Patient.java
@@ -5,6 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -24,7 +25,7 @@ public class Patient {
     @NotBlank
     private String lastName;
 
-    @NotBlank
+    @NotNull
     private LocalDate dateOfBirth;
 
     @NotBlank
